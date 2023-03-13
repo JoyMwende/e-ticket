9561ac4805836f8d9857764dfee5ec7490091127
Joy Mwende Kairi <94088942+JoyMwende@users.noreply.github.com>
2023-03-13 12:12:19 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const pino = require("express-pino-logger")();
+require("dotenv").config();
+const cors = require("cors");
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require("twilio")(accountSid, authToken);
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(pino);
+
+app.use(express.json());
+app.use(cors());
+
+
+// app.get("/api/greeting", (req, res) => {
+//   const name = req.query.name || "World";
+//   res.setHeader("Content-Type", "application/json");
+//   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+// });
+
+app.post("/api/messages", (req, res) => {
+  res.header("Content-Type", "application/json");
+  client.messages
+    .create({
+      from: process.env.TWILIO_PHONE_NUMBER,
+      to: `${req.body.to}`,
+      body: `${req.body.message}`,
+    })
+    .then(() => {
+      res.send(JSON.stringify({ success: true }));
+      console.log("Message sent succeessfully!")
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send(JSON.stringify({ success: false }));
+      console.log("Message not sent!")
+    });
+});
+
+app.listen(3002, () =>
+  console.log("Express server is running on localhost:3002")
+);
